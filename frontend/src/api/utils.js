05e7b358008c741c9c03bfd78d719dda4876d920
DaveSharma-Hub <davesharma12345@outlook.com>
2023-11-11 15:54:30 -0700

    backend

--- frontend/src/api/utils.js
+++ frontend/src/api/utils.js
@@ -4,17 +4,26 @@ import axios from 'axios';
 async function getFoodVisorData(){
     const options = {
         method: 'POST',
-        url: 'https://vision.foodvisor.io/api/1.0/en',
+        url: 'https://vision.foodvisor.io/api/1.0/en/analysis/',
         headers: {
-            'Authorization': 'Api-Key L49xZqGL.hLM1CxkTdNhhdZJILaCC0pnh29sFQTAF',
+            'Content-Type':'application/x-www-form-urlencoded',
+            'Authorization': 'Api-Key codethechangefourthfloorapi',
         },
         data:{
             'image':'https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGYtczg3LW1uLTI1LTAxLnBuZw.png'
         }
     };
+    const options2 = {
+        method: 'GET',
+        url: 'https://vision.foodvisor.io/api/1.0/en/food/list',
+        headers: {
+            'Content-Type':'application/x-www-form-urlencoded',
+            'Authorization': 'Api-Key L49xZqGL.hLM1CxkTdNhhdZJILaCC0pnh29sFQTAF',
+        }
+    };
 
     try{
-        return await axios.request(options);
+        return await axios.request(options2);
     }catch(e){
         console.log(e);
     }
