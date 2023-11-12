c7fa0934c62d3679a2c6e686b1d7b1c253bc80b6
DaveSharma-Hub <davesharma12345@outlook.com>
2023-11-12 10:08:22 -0700

    styling css

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -4,6 +4,8 @@ import Row from "react-bootstrap/esm/Row";
 import Card from "react-bootstrap/Card";
 import Tooltip from "@mui/material/Tooltip";
 import { Button } from "@mui/material";
+import './results.css';
+
 // eslint-disable-next-line react/prop-types
 export default function Results({ data, imgSrc }) {
   return (
@@ -18,8 +20,10 @@ export default function Results({ data, imgSrc }) {
       <Row>
         <Col xs="6" md="6" lg="6" style={{}}>
           <Row>
-            <h2>{data.predicted?.predictedLabel}</h2>
-            <img src={imgSrc} style={{ borderRadius: "20px" }} />
+            <div className="imageClass">
+              <h2>{data.predicted?.predictedLabel}</h2>
+              <img src={imgSrc} style={{ borderRadius: "20px", width:'40vw' }} />
+            </div>
           </Row>
           <Tooltip
             title="Find foods in the same category that have better carbon footprints"
@@ -30,45 +34,50 @@ export default function Results({ data, imgSrc }) {
           <Row
             style={{ display: "grid", gridTemplateColumns: "auto auto auto" }}
           >
-            {
-              // eslint-disable-next-line react/prop-types
-              data.betterFootprints?.map(
-                (
-                  { category, footprint, group, name, rating_quality },
-                  index
-                ) => {
-                  return (
-                    <Card key={index} style={{ width: "18rem" }}>
-                      <Card.Body>
-                        <Card.Title>{name}</Card.Title>
-                        <Card.Text>{category}</Card.Text>
-                        <Card.Text>{footprint}</Card.Text>
-                        <Card.Text>{rating_quality}</Card.Text>
-                      </Card.Body>
-                    </Card>
-                  );
-                }
-              )
-            }
+            <div className="alternativeClass">
+              {
+                // eslint-disable-next-line react/prop-types
+                data.betterFootprints?.map(
+                  (
+                    { category, footprint, group, name, rating_quality },
+                    index
+                  ) => {
+                    return (
+                      <Card key={index} style={{ width: "18rem" }}>
+                        <Card.Body>
+                          <Card.Title>{name}</Card.Title>
+                          <Card.Text>{category}</Card.Text>
+                          <Card.Text>{footprint}</Card.Text>
+                          <Card.Text>{rating_quality}</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    );
+                  }
+                )
+              }
+
+            </div>
           </Row>
         </Col>
         <Col>
           <Row className="justify-content-md-center">
             <Col xs="3" md="6" lg="6">
-              <h4>Food Recognized as</h4>
-              {data.predicted?.topScores?.map(({ label, score }, index) => {
-                return (
-                  <Card
-                    key={index}
-                    style={{ width: "15rem", marginBottom: "15px" }}
-                  >
-                    <Card.Body>
-                      <Card.Title>Prediction: {label}</Card.Title>
-                      <Card.Text>Confidence: {score}</Card.Text>
-                    </Card.Body>
-                  </Card>
-                );
-              })}
+              <div className="predictionClass">
+                <h4>Food Recognized as</h4>
+                {data.predicted?.topScores?.map(({ label, score }, index) => {
+                  return (
+                    <Card
+                      key={index}
+                      style={{ width: "15rem", marginBottom: "15px" }}
+                    >
+                      <Card.Body>
+                        <Card.Title>Prediction: {label}</Card.Title>
+                        <Card.Text>Confidence: {score}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  );
+                })}
+              </div>
             </Col>
             <Col
               xs="6"
@@ -76,44 +85,47 @@ export default function Results({ data, imgSrc }) {
               lg="6"
               style={{ height: "80vh", overflowY: "auto" }}
             >
-              <h4>Carbon footprint information:</h4>
-              {
-                // eslint-disable-next-line react/prop-types
-                data.foodprint?.map(
-                  (
-                    { category, footprint, group, name, rating_quality },
-                    index
-                  ) => {
-                    return (
-                      <Card
-                        key={index}
-                        style={{ width: "15rem", marginBottom: "15px" }}
-                      >
-                        <Card.Body>
-                          <Card.Title>Name: {name}</Card.Title>
-                          <Card.Text>Food Category: {category}</Card.Text>
-                          <Card.Text>
-                            <Tooltip
-                              title="Computed carbon footprint for the specific product"
-                              arrow
-                            >
-                              Foodprint: {footprint} <i> CO2 eq/kg </i>
-                            </Tooltip>
-                          </Card.Text>
-                          <Card.Text>
-                            <Tooltip
-                              title="Rating quality of footprint field 1:highest, 5: lowest"
-                              arrow
-                            >
-                              Rating quality: {rating_quality}
-                            </Tooltip>
-                          </Card.Text>
-                        </Card.Body>
-                      </Card>
-                    );
-                  }
-                )
-              }
+              <div className="carbonClass">
+                <h4>Carbon footprint information:</h4>
+                {
+                  // eslint-disable-next-line react/prop-types
+                  data.foodprint?.map(
+                    (
+                      { category, footprint, group, name, rating_quality },
+                      index
+                    ) => {
+                      return (
+                        <Card
+                          key={index}
+                          style={{ width: "15rem", marginBottom: "15px" }}
+                        >
+                          <Card.Body>
+                            <Card.Title>Name: {name}</Card.Title>
+                            <Card.Text>Food Category: {category}</Card.Text>
+                            <Card.Text>
+                              <Tooltip
+                                title="Computed carbon footprint for the specific product"
+                                arrow
+                              >
+                                Foodprint: {footprint} <i> CO2 eq/kg </i>
+                              </Tooltip>
+                            </Card.Text>
+                            <Card.Text>
+                              <Tooltip
+                                title="Rating quality of footprint field 1:highest, 5: lowest"
+                                arrow
+                              >
+                                Rating quality: {rating_quality}
+                              </Tooltip>
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      );
+                    }
+                  )
+                }
+
+              </div>
             </Col>
           </Row>
         </Col>
